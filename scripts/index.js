faa0555e5872de74d70be6181739d99ccf712279
Sean Cahill <seancahill25@gmail.com>
2017-11-20 17:48:44 +0000

    Hotel buttons working

--- scripts/index.js
+++ scripts/index.js
@@ -11,11 +11,29 @@ var hotel = {
             available: 20
         },
         {
-            name: "Luxery Suite",
+            name: "Luxury Suite",
             price: "$250.00",
             available: 5
         }
     ],
     name: "CareerDevs Innt"
 }
+// console.log(document.getElementById("label").nodeName);
 
+for (var i =0; i < hotel.rooms.length; i++){
+    console.log("start");
+    var radioBtn = document.createElement("INPUT");
+    var radioLbl = document.createElement("LABEL");
+    radioBtn.setAttribute("type", "radio");
+    radioBtn.setAttribute("name" , "rooms");
+    radioBtn.setAttribute("value" , i);
+    radioBtn.setAttribute("id", "rooms" + i);
+    radioLbl.innerHTML = hotel.rooms[i].name;
+    console.log("stop");
+    
+    document.getElementById("radialSection").appendChild(radioBtn);
+    document.getElementById("radialSection").appendChild(radioLbl);
+}
+//display rooms as radio options
+// on form submission confirm rado was selected
+// and checkmark checked
